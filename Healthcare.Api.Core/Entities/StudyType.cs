c6a6ed833985d1717fd153da70606171ff4a887d
DESKTOP-MJV996 <martinjvidal96@gmail.com>
2024-03-25 20:19:27 -0300

    news

---
+++ Healthcare.Api.Core/Entities/StudyType.cs
@@ -0,0 +1,9 @@
+﻿namespace Healthcare.Api.Core.Entities
+{
+    public class StudyType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Study> Studies { get; set; }
+    }
+}
---
+++ Healthcare.Api.Repository/EntitiesConfiguration/StudyEntityConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Healthcare.Api.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Healthcare.Api.Repository.EntitiesConfiguration
+{
+    public class StudyEntityConfiguration : IEntityTypeConfiguration<Study>
+    {
+        public void Configure(EntityTypeBuilder<Study> builder)
+        {
+            builder.ToTable("Study").HasKey(x => x.Id);
+            builder.Property(s => s.Id).ValueGeneratedOnAdd();
+
+            builder.Property(s => s.Date).IsRequired();
+            builder.Property(s => s.LocationS3).IsRequired();
+            builder.Property(s=> s.Note).IsRequired(false);
+
+            builder.HasOne(s => s.StudyType)
+               .WithOne()
+               .HasForeignKey<Study>(s => s.StudyTypeId)
+               .IsRequired();
+
+            builder.HasOne(e => e.StudyType)
+               .WithMany()
+               .HasForeignKey(e => e.StudyTypeId);
+
+            builder.HasOne(s => s.Patient)
+                .WithMany(s => s.Studies)
+                .HasForeignKey(s => s.PatientId)
+                .IsRequired();
+        }
+    }
+}
