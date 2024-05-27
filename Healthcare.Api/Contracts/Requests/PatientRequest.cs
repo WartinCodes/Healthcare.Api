03edcc32b15fd7b9ce0841c7f8eaf562a05f9714
mvidal <martinjvidal96@gmail.com>
2024-05-27 19:46:59 -0300

    mail nullablwe

--- Healthcare.Api/Contracts/Requests/PatientRequest.cs
+++ Healthcare.Api/Contracts/Requests/PatientRequest.cs
@@ -5,7 +5,7 @@
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
+        public string? Email { get; set; }
         public string PhoneNumber { get; set; }
         public DateTime BirthDate { get; set; }
         public string Photo { get; set; }
