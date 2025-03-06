aa6513eccd78f56a6bd95d7867351687d26ed5ab
mvidal <martinjvidal96@gmail.com>
2025-03-06 18:54:33 -0300

    New AddressController, endpoint Create

---
+++ Healthcare.Api/Controllers/AddressController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Healthcare.Api.Contracts.Requests;
+using Healthcare.Api.Contracts.Responses;
+using Healthcare.Api.Core.Entities;
+using Healthcare.Api.Core.ServiceInterfaces;
+using Healthcare.Api.Service.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Healthcare.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+        private readonly IAddressService _addressService;
+        private readonly IMapper _mapper;
+
+        public AddressController(IAddressService addressService, IMapper mapper)
+        {
+            _addressService = addressService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BloodTestResponse>> Create([FromBody] AddressRequest request)
+        {
+            Address newAddress = await _addressService.Add(_mapper.Map<Address>(request));
+            return Ok(newAddress);
+        }
+    }
+}
