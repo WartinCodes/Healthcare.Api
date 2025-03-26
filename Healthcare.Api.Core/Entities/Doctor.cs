8a0671a2c27d83008ef1580e7e4525e32dd212da
mvidal <martinjvidal96@gmail.com>
2025-03-26 17:06:18 -0300

    FDE-94 : Add new column SignedDoctorId in Study entity.

--- Healthcare.Api.Core/Entities/Doctor.cs
+++ Healthcare.Api.Core/Entities/Doctor.cs
@@ -1,26 +1,29 @@
-﻿namespace Healthcare.Api.Core.Entities
+﻿using Healthcare.Api.Core.Entities;
+
+public class Doctor
 {
-    public class Doctor
+    public Doctor()
     {
-        public Doctor()
-        {
-            
-        }
-
-        public Doctor(int userId, ICollection<Speciality> doctorSpecialities, ICollection<HealthInsurance> healthInsurances)
-        {
-            UserId = userId;
-            Specialities = doctorSpecialities ?? new HashSet<Speciality>();
-            HealthInsurances = healthInsurances ?? new HashSet<HealthInsurance>();
-        }
-
-        public int Id { get; set; }
-        public int UserId { get; set; }
-        public virtual User User { get; set; }
-        public string Matricula { get; set; }
-        public string? Firma { get; set; }
-        public string? Sello { get; set; }
-        public virtual ICollection<Speciality> Specialities { get; set; }
-        public virtual ICollection<HealthInsurance> HealthInsurances { get; set; }
+        Specialities = new HashSet<Speciality>();
+        HealthInsurances = new HashSet<HealthInsurance>();
+        SignedStudies = new HashSet<Study>();
     }
+
+    public Doctor(int userId, ICollection<Speciality> doctorSpecialities, ICollection<HealthInsurance> healthInsurances)
+    {
+        UserId = userId;
+        Specialities = doctorSpecialities ?? new HashSet<Speciality>();
+        HealthInsurances = healthInsurances ?? new HashSet<HealthInsurance>();
+        SignedStudies = new HashSet<Study>();
+    }
+
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public virtual User User { get; set; }
+    public string Matricula { get; set; }
+    public string? Firma { get; set; }
+    public string? Sello { get; set; }
+    public ICollection<Speciality> Specialities { get; set; }
+    public ICollection<HealthInsurance> HealthInsurances { get; set; }
+    public ICollection<Study> SignedStudies { get; set; }
 }
